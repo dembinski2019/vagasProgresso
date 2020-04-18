d7cb6377428b39e0f731876d6a76ea893c019e6f
dembinski2019 <everton_s.d@hotmail.com>
2020-04-18 01:32:07 -0300

    correcao teste

--- searchJobs/urls.py
+++ searchJobs/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
 from django.conf.urls.static import static
 
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path('contas/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
