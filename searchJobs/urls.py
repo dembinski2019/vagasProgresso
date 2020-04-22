54129fea1118d359f320ae1d57ba6fa4dd2c44be
dembinski2019 <everton_s.d@hotmail.com>
2020-04-22 15:05:48 -0300

    teste

--- searchJobs/urls.py
+++ searchJobs/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('contas/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
