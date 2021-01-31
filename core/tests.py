7d45b6b27e6171c6263f0132dd28b16bc7329ebd
everton <everton_s.d@hotmail.com>
2021-01-31 18:08:16 -0300

    adding code tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Employtment
+from accounts.models import User
 
-# Create your tests here.
+class EmploytmentModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username="company2", password="Dembinski123")
+
+    def test_new_employtment_ok(self):
+        for i in range(3):
+                self.new_employtment = Employtment.objects.create(
+                    employtment_name=f"Motorista{i}",
+                    requirements="Cnh C, precisa conhecer bem a cidade",
+                    description="Fazer entregas na cidade, horario 07:00 as 18:00",
+                    salary='1280',
+                    jobs_vacancy="3",
+                    company=self.user,
+                    )
+        self.count = Employtment.objects.count()
+        self.assertEqual(self.count, 3)
+
+    def tearDown(self):
+        Employtment.objects.all().delete()
+
+        
--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+class WebSiteIndexTest(TestCase):
+
+    def setUp(self):
+        self.resp = self.client.get('/')
+
+    def test_get_index(self):
+        """GET / must return status code 200 """
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_template_index(self):
+        """Must use template website/index.html"""
+        self.assertTemplateUsed(self.resp, "website/index.html")
+
+class WebSiteCandidateTest(TestCase):
+
+    def setUp(self):
+        self.resp = self.client.get('/candidato')
+
+    def test_get_candidate(self):
+        """GET /candidato must return status code 200 """
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_template_candidate(self):
+        """Must use template website/candidate_home.html"""
+        self.assertTemplateUsed(self.resp, "website/candidate_home.html")
+
+class WebSiteCompanyTest(TestCase):
+    
+    def setUp(self):
+        self.resp = self.client.get('/empresas')
+
+    def test_get_company(self):
+        """GET /empresas must return status code 200 """
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_template_company(self):
+        """Must use template website/company_home.html"""
+        self.assertTemplateUsed(self.resp, "website/company_home.html")
